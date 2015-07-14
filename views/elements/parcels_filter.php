47c92b8c5f3b44028bed7e35c692588823eb9e3f
Olajide <olajide.oye@gmail.com>
2015-07-14 19:46:19 +0100

    added datepicker to parcel filter element
    used js to hide the manage addresses link

--- views/elements/parcels_filter.php
+++ views/elements/parcels_filter.php
@@ -1,12 +1,16 @@
+<?php
+	use yii\helpers\Html;
+?>
+<?= Html::cssFile('@web/css/libs/datepicker.css') ?>
 <div class="clearfix">
 	<div class="pull-left">
 		<label for="">From:</label><br>
-		<input name="" id="" class="form-control date-range">
+		<input name="" id="" class="form-control date-range" data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
 	</div>
 
 	<div class="pull-left">
 		<label for="">To:</label><br>
-		<input name="" id="" class="form-control date-range">
+		<input name="" id="" class="form-control date-range"  data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
 	</div>
 	<div class="pull-left">
 		<label for="">Filter status</label><br>
@@ -17,3 +21,7 @@
 		<button class="btn btn-default"><i class="fa fa-search"></i></button>
 	</div>
 </div>
+
+<?php
+$this->registerJsFile('@web/js/libs/bootstrap-datepicker.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+?>
