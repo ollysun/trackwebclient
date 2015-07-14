cb59517f432bb934ae2af4f99d8f88ecac0a81a3
Akintewe Oluwarotimi <akintewe.rotimi@gmail.com>
2015-07-14 20:56:09 +0100

    phone number search

--- adapter/UserAdapter.php
+++ adapter/UserAdapter.php
@@ -2,7 +2,13 @@
 
 namespace Adapter;
 
+use Adapter\BaseAdapter;
+use Adapter\Globals\ServiceConstant;
 
 class UserAdapter extends BaseAdapter {
 
+    public function getUserDetails($term)
+    {
+        return $this->request(ServiceConstant::URL_USER_BY_PHONE, [ 'phone' => $term ], self::HTTP_GET);
+    }
 }
