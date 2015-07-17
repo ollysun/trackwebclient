ce0c371efbd4352f516d67943161e1075c8f7715
RichardBoyewa <richardboyewa@gmail.com>
2015-07-17 05:33:18 +0100

    Adding default date to date filter - conflict fixes

--- views/elements/parcels_filter.php
+++ views/elements/parcels_filter.php
@@ -7,16 +7,16 @@ use Adapter\Globals\ServiceConstant;
 ?>
     <form>
         <div class="clearfix">
-            <div class="pull-left">
+            <div class="pull-left form-group form-group-sm">
                 <label for="">From:</label><br>
                 <input name="from" id="" class="form-control date-range" value="<?= date('Y/m/d')?>" data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
             </div>
 
-            <div class="pull-left">
+            <div class="pull-left form-group form-group-sm">
                 <label for="">To:</label><br>
                 <input name="to" id="" class="form-control date-range"  value="<?= date('Y/m/d')?>" data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
             </div>
-            <div class="pull-left">
+            <div class="pull-left form-group form-group-sm">
                 <label for="">Filter status</label><br>
                 <select name="date_filter" id="" class="form-control  filter-status">
                     <option value="-1">NOT APPLICABLE</option>
@@ -32,7 +32,7 @@ use Adapter\Globals\ServiceConstant;
             </div>
             <div class="pull-left">
                 <label>&nbsp;</label><br>
-                <button class="btn btn-default"><i class="fa fa-search"></i></button>
+                <button class="btn btn-default btn-sm"><i class="fa fa-search"></i></button>
             </div>
         </div>
     </form>
