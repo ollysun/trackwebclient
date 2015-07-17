a71c2483cbd7dcf3db078b8044a8d9c6a182006c
RichardBoyewa <richardboyewa@gmail.com>
2015-07-17 05:08:13 +0100

    Adding default date to date filter

--- views/elements/parcels_filter.php
+++ views/elements/parcels_filter.php
@@ -9,12 +9,12 @@ use Adapter\Globals\ServiceConstant;
         <div class="clearfix">
             <div class="pull-left">
                 <label for="">From:</label><br>
-                <input name="from" id="" class="form-control date-range" data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
+                <input name="from" id="" class="form-control date-range" value="<?= date('Y/m/d')?>" data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
             </div>
 
             <div class="pull-left">
                 <label for="">To:</label><br>
-                <input name="to" id="" class="form-control date-range"  data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
+                <input name="to" id="" class="form-control date-range"  value="<?= date('Y/m/d')?>" data-provide="datepicker" data-date-format="yyyy/mm/dd" data-date-end-date="0d">
             </div>
             <div class="pull-left">
                 <label for="">Filter status</label><br>
