c183f159e5c9838404bdd38d6298594693bb6e22
RichardBoyewa <richardboyewa@gmail.com>
2015-07-18 02:03:43 +0100

    Waybill generation  - CPTT-47

--- web/js/barcode.js
+++ web/js/barcode.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $("#barcode").barcode(waybill,"code128");
+    html2canvas(document.body, {
+        onrendered: function(canvas) {
+            document.body.appendChild(canvas);
+        },
+        width: 300,
+        height: 300
+    });
 });
