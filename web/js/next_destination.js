3232a0a9aa9697670b1aac70a391d079f3afaa2e
Akintewe Oluwarotimi <akintewe.rotimi@gmail.com>
2015-07-22 05:10:36 +0100

    selecting and deselecting of destination branches

--- web/js/next_destination.js
+++ web/js/next_destination.js
@@ -11,13 +11,114 @@ var Parcel_Destination = {
     fillSelectOption: function(url, param, selectSelector) {
         $.get( url, param, function(response){
             if(response.status === 'success') {
-                var html = '<option>Select Name...</option>';
+                var html = '<option value="">Select Name...</option>';
                 $.each(response.data, function(i, item) {
                     html += "<option value='" + item.id + "'>" + item.name.toUpperCase() + "</option>";
                 });
                 $(selectSelector).html(html);
             }
         });
+    }
+};
+
+var TableHelper = {
+
+
+    renumberTableSerialNo: function (tableSelector, colIndex) {
+
+
+        var trs = $(tableSelector + ' tbody').children();
+        if(!this.isDefined(colIndex)) {
+            colIndex = 0;
+        }
+        for(var i = 0; i < trs.length; i++) {
+            var children = $(trs[i]).find('td:eq(' + colIndex + ')').first().html(i+1);
+        }
+    },
+
+    isItemInTableColumn: function(tableSelector, colIndex, searchItem) {
+
+        var tempText = '';
+        var found = false;
+        var trs = $(tableSelector + ' tbody').children();
+        if(!this.isDefined(colIndex)) {
+            colIndex = 0;
+        }
+
+        $.each(trs, function(index, tr){
+            tempText = $.trim($(tr).children().eq(colIndex).html());
+            if((tempText) == searchItem) {
+                found = true;
+            }
+        });
+        return found;
+    },
+
+    getColumTextData: function(tableSelector, colIndex) {
+
+        var items = [];
+        var trs = $(tableSelector + ' tbody').children();
+        if(!this.isDefined(colIndex)) {
+            colIndex = 0;
+        }
+
+        $.each(trs, function(index, tr){
+            items.push($.trim($(tr).children().eq(colIndex).html()));
+        });
+        return items;
+    },
+
+    setCellData: function(tableSelector, rowIndex, colIndex, content) {
+
+        if(!this.isDefined(rowIndex) || !this.isDefined(colIndex)) {
+            return;
+        }
+        var trs = $(tableSelector + ' tbody').children();
+        $(trs).eq(rowIndex).children().eq(colIndex).html(content);
+    },
+
+    setColumnData: function(tableSelector, colIndex, content, withCheckbox) {
+
+        if(!this.isDefined(colIndex)) {
+            return;
+        }
+        var trs = $(tableSelector + ' tbody').children();
+
+        $.each(trs, function(rowIndex, tr){
+            if(withCheckbox) {
+                var chkValue = $(tr).children().find(':checkbox').attr('value');
+                if(chkValue) {
+                    $(tr).children().eq(colIndex).html(content);
+                }
+            } else {
+                $(tr).children().eq(colIndex).html(content);
+            }
+        });
+    },
+
+    getCellData: function(tableSelector, colIndex, rowIndex) {
+        var cellData = '';
+        var trs = $(tableSelector + ' tbody').children();
+        if(!this.isDefined(colIndex)) {
+            colIndex = 0;
+        }
+
+        $.each(trs, function(index, tr){
+            if(index === rowIndex) {
+                cellData = $.trim($(tr).children().eq(colIndex).html());
+            }
+        });
+
+        return cellData;
+    },
+
+    isCallback: function(callback) {
+        return (callback && typeof(callback) === typeof(Function));
+    },
+
+    isDefined: function (value) {
+
+        return (typeof value !== 'undefined');
     }
 };
 
@@ -39,16 +140,23 @@ $(document).ready(function(){
     });
 
     $('.chk_next').on('click', function(event){
-        var waybill = $(this).closest('tr').attr('data-waybill');
+        var tr = $(this).closest('tr');
+        var rowIndex = $(tr).index();
+        var waybill = $(tr).attr('data-waybill');
+        var dest_value = $('#branch_name').val();
+        var destination = (dest_value !== '') ? $('#branch_name').find('option:selected').text() : '';
         var curr = $(this).attr('value');
         if(curr) {
             if(curr === waybill) {
                 $(this).removeAttr('value');
+                TableHelper.setCellData('#next_dest', rowIndex, 4, '');
             } else {
                 $(this).attr('value', waybill);
+                TableHelper.setCellData('#next_dest', rowIndex, 4, destination);
             }
         } else {
             $(this).attr('value', waybill);
+            TableHelper.setCellData('#next_dest', rowIndex, 4, destination);
         }
     });
 
@@ -67,5 +175,21 @@ $(document).ready(function(){
             event.preventDefault();
             return;
         }
+
+        var dest_value = $('#branch_name').val();
+        if(dest_value == '') {
+
+            alert('Please select a ' + $('#hub_branch_label').html() + ' to proceed.');
+            event.preventDefault();
+            return;
+        }
+    });
+
+    $('#branch_name').on('change', function(){
+        var name = $(this).val();
+        if(name !== '') {
+            name = $(this).find('option:selected').text();
+        }
+        TableHelper.setColumnData('#next_dest', 4, name, true);
     });
 });
