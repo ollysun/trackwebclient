c4412e4939e28c086e45032012d38a718edebe5a
Olajide <olajide.oye@gmail.com>
2015-07-24 12:15:08 +0100

    minor changes to on forwarding and state mapping table... as well as style changes

--- views/site/billing_onforwarding.php
+++ views/site/billing_onforwarding.php
@@ -3,7 +3,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 
 /* @var $this yii\web\View */
-$this->title = 'Billing Onforwarding';
+$this->title = 'Billing: Onforwarding Charges';
 $this->params['breadcrumbs'] = array(
 	array('label'=> 'Onforwarding Charges')
 );
