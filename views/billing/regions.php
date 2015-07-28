e104908581e552be8c1552ba127ea803bd2d9b00
Olawale Lawal <lawalolawale@gmail.com>
2015-07-28 12:12:36 +0100

    Merge branch 'develop' of https://bitbucket.org/cottacush/courierplusng into features/regions_creation-CPTTK-108
    Conflicts:
    	controllers/BillingController.php

--- views/billing/regions.php
+++ views/billing/regions.php
@@ -82,7 +82,6 @@ $this->params['content_header_button'] = '<button type="button" class="btn btn-p
     </div>
 </div>
 
-
 <!-- Modal -->
 <div class="modal fade" id="region_myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
@@ -102,32 +101,12 @@ $this->params['content_header_button'] = '<button type="button" class="btn btn-p
                         <label>Description</label>
                         <textarea class="form-control" name="description"></textarea>
                     </div>
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <div class="form-group">
-                                <label for="">Country</label>
-                                <select name="country_id" id="country_id" class="form-control required">
-                                    <option value="">Choose One</option>
-                                    <?php
-                                    if (isset($countries) && is_array(($countries))):
-                                        foreach ($countries as $country) {
-                                            ?>
-                                            <option
-                                                value="<?= $country['id']; ?>"><?= ucwords($country['name']); ?></option>
-                                        <?php }
-                                    endif; ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="form-group">
-                                <label for="">Activate Region?</label>
-                                <select name="status" class="form-control required">
-                                    <option value="<?= ServiceConstant::ACTIVE; ?>">Yes</option>
-                                    <option value="<?= ServiceConstant::INACTIVE; ?>">No</option>
-                                </select>
-                            </div>
-                        </div>
+                    <div class="form-group">
+                        <label for="">Activate Region?</label>
+                        <select class="form-control required" name="status">
+                            <option value="<?= ServiceConstant::ACTIVE; ?>">Yes</option>
+                            <option value="<?= ServiceConstant::INACTIVE; ?>">No</option>
+                        </select>
                     </div>
                 </div>
                 <div class="modal-footer">
