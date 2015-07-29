b21d203da9e4cca30f4e8fc7db0f37cd7c315467
RichardBoyewa <richardboyewa@gmail.com>
2015-07-29 08:16:45 +0100

    Staff Management fixes

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -599,7 +599,20 @@ class SiteController extends BaseController
         $roles = new ResponseHandler($roles);
         $state_list = $states->getStatus()==ResponseHandler::STATUS_OK?$states->getData(): [];
         $role_list =  $roles->getStatus()==ResponseHandler::STATUS_OK?$roles->getData(): [];
-        return $this->render('managestaff',['states' => $state_list,'roles'=> $role_list]);
+
+        $staffMembers = [];
+        $staffAdp = new AdminAdapter(RequestHelper::getClientID(),RequestHelper::getAccessToken());
+        if(isset(Calypso::getInstance()->get()->search) && strlen(Calypso::getInstance()->get()->search) > 0){
+            $is_email = !(filter_var(Calypso::getInstance()->get()->search,FILTER_VALIDATE_EMAIL) === false);
+            $staff_data = $staffAdp->searchStaffMembers(Calypso::getInstance()->get()->search,$is_email,$offset,$this->page_width);
+        }else {
+            $staff_data = $staffAdp->getStaffMembers($offset, $this->page_width, $role);
+        }
+        $resp = new ResponseHandler($staff_data);
+        $staffMembers = $resp->getData();
+
+
+        return $this->render('managestaff',['states' => $state_list,'roles'=> $role_list,'staffMembers' => $staffMembers,'offset'=>$offset,'role'=>$role,'page_width'=>$this->page_width]);
     }
     public function actionHubarrival()
     {
