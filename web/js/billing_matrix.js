4387d4708d445f8093cdd2544b6914e1112e3149
RichardBoyewa <richardboyewa@gmail.com>
2015-08-05 04:27:36 +0100

    Matrix view - Matrix edit modal - Billing zones listing, removal of mapping from hubs

--- web/js/billing_matrix.js
+++ web/js/billing_matrix.js
@@ -24,10 +24,30 @@ $(document).ready(function() {
             $("#to").val(to_id);
             $("#to_text").val(d.to_branch.name + ' - '+d.to_branch.code);
             $("#from_text").val(d.from_branch.name + ' - '+d.from_branch.code);
+            $("#zone_mapping_id").val(d.id);
+            $("#zone_id").val(d.zone_id);
             $('#editModal').modal();
         }catch(e){
             console.log(e);
         }
 
     });
+    $("#remove_mapping").unbind("click").on("click",function(){
+        var data = $("#update_zone_mapping_form").serialize();
+        if(confirm("Are you sure you want to remove this mapping?")){
+            Hub.postToServer("removemapping",data,function(response){
+                window.location.reload();
+            });
+        }
+    });
+    $("#update_mapping").unbind("click").on("click",function(){
+        var that = this;
+        $(that).html("Updating... Please wait.");
+        var data = $("#update_zone_mapping_form").serialize();
+        Hub.postToServer("updatemapping",data,function(response){
+            window.location.reload();
+        });
+
+    });
+
 } );
