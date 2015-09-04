cfa8e9ea604d8e1f3f4b71fc0769d9ea51f59696
Olajide <olajide.oye@gmail.com>
2015-09-04 12:38:28 +0100

    Teller UI

--- views/shipments/all.php
+++ views/shipments/all.php
@@ -37,7 +37,7 @@ if($search){
 <?= Html::cssFile('@web/css/libs/dataTables.tableTools.css') ?>
 
 <?php
-//$this->params['content_header_button'] = $this->render('../elements/content_header_new_parcel_button');
+    $this->params['content_header_button'] = '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#teller-modal">Submit Teller</button>';
 ?>
 
 <?php echo Calypso::showFlashMessages(); ?>
@@ -132,13 +132,68 @@ if($search){
 </div>
 
 
+<div class="modal fade" id="teller-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <form method="post" action="" class="validate-form">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Submit Teller Details</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-xs-6 form-group">
+                            <label for="">Bank</label>
+                            <select class="form-control validate required"></select>
+                        </div>
+                        <div class="col-xs-6 form-group">
+                            <label for="">Account no</label>
+                            <input type="text" class="form-control validate required non-zero-integer">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 form-group">
+                            <label for="">Teller no</label>
+                            <input type="text" class="form-control validate required non-zero-integer">
+                        </div>
+                        <div class="col-xs-6 form-group">
+                            <label for="">Amount paid</label>
+                            <div class="input-group">
+                                <span class="input-group-addon currency naira"></span>
+                                <input type="text" class="form-control validate required non-zero-number">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Teller Snapshot (optional)</label>
+                        <input type="file" class="form-control">
+                    </div>
+
+                    <hr />
+                    <table class="table table-bordered table-condensed" id="teller-modal-table">
+                        <thead>
+                        <tr>
+                            <th>S/N</th>
+                            <th>Waybill No.</th>
+                            <th>Sender name</th>
+                        </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" id="btnGenerate">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
 <!-- this page specific scripts -->
-<?php $this->registerJsFile('@web/js/libs/jquery.dataTables.js', ['depends' => [\yii\web\JqueryAsset::className()]]); ?>
-<?php $this->registerJsFile('@web/js/libs/dataTables.fixedHeader.js', ['depends' => [\yii\web\JqueryAsset::className()]]); ?>
-<?php $this->registerJsFile('@web/js/libs/dataTables.tableTools.js', ['depends' => [\yii\web\JqueryAsset::className()]]); ?>
-<?php $this->registerJsFile('@web/js/libs/jquery.dataTables.bootstrap.js', ['depends' => [\yii\web\JqueryAsset::className()]]); ?>
 <?php $this->registerJsFile('@web/js/hub_util.js', ['depends' => [\app\assets\AppAsset::className()]])?>
 <?php $this->registerJsFile('@web/js/jquery.dataTables.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]) ?>
 <?php $this->registerJsFile('@web/js/dataTables.bootstrap.js', ['depends' => [\yii\web\JqueryAsset::className()]]) ?>
 <?php $this->registerJsFile('@web/js/table.js', ['depends' => [\yii\web\JqueryAsset::className()]]) ?>
+<?php $this->registerJsFile('@web/js/validate.js', ['depends' => [\app\assets\AppAsset::className()]])?>
 
