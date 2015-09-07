ef5c57f1ece71997bb219b302fac873fd59abc30
Olajide <olajide.oye@gmail.com>
2015-09-07 13:22:07 +0100

    CPTTK-202 Added Age analysis columns to views

--- views/hubs/delivery.php
+++ views/hubs/delivery.php
@@ -103,6 +103,7 @@ $this->params['breadcrumbs'] = array(
                             <th>Next Destination</th>
                             <th>Final Destination</th>
                             <th>Weight (Kg)</th>
+                            <th>Age analysis</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -123,6 +124,7 @@ $this->params['breadcrumbs'] = array(
                                 echo "<td>" . strtoupper(Calypso::getValue($parcels, 'to_branch.name')) ."</td>";
                                 echo "<td>" . ucwords(Calypso::getValue($parcels, 'receiver_address.city.name') . ', ' . Calypso::getValue($parcels, 'receiver_address.state.name')) . "</td>";
                                 echo "<td>" . Calypso::getValue($parcels, 'weight') . "</td>";
+                                echo "<td></td>";
                                 echo "</tr>";
                             }
                         ?>
