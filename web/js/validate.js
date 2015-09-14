f46f074932c5c1c556a672892ec0bfd92506cf65
Olajide <olajide.oye@gmail.com>
2015-09-14 12:06:20 +0100

    Removed front end validation on email not being compulsory

--- web/js/validate.js
+++ web/js/validate.js
@@ -23,88 +23,92 @@ function validateFxn() {
     var msg = '';
 
     formgroup.removeClass('has-error has-success').find('.help-block.help-validation-error').remove();
-    if(input.hasClass('required') && val === '')
+    if(val === '')
     {
-        msg = 'Required field';
-        isValid = false;
-    }
-    else if(input.hasClass('email'))
-    {
-        var em = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,12}(?:\.[a-z]{2})?)$/i;
-        if(!em.test(val))
-        {
-            msg = 'Invalid entry';
+        if (input.hasClass('required')) {
+            msg = 'Required field';
             isValid = false;
         }
     }
-    else if(input.hasClass('integer'))
+    else
     {
-        var test = /^[-+]?\d+$/;
-        if(!test.test(val))
+        if(input.hasClass('email'))
         {
-            msg = 'Invalid entry';
+            var em = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,12}(?:\.[a-z]{2})?)$/i;
+            if(!em.test(val))
+            {
+                msg = 'Invalid entry';
+                isValid = false;
+            }
+        }
+        else if(input.hasClass('integer'))
+        {
+            var test = /^[-+]?\d+$/;
+            if(!test.test(val))
+            {
+                msg = 'Invalid entry';
+                isValid = false;
+            }
+        }
+        else if(input.hasClass('non-zero-integer'))
+        {
+            var test = /^[1-9]\d*$/;
+            if(!test.test(val))
+            {
+                msg = 'Invalid entry';
+                isValid = false;
+            }
+        }
+        else if(input.hasClass('non-zero-number'))
+        {
+            var test = /^(?=.*[1-9])\d+(\.\d+)?$/;
+            if(!test.test(val) && val !== "")
+            {
+                msg = 'Invalid entry';
+                isValid = false;
+            }
+        }
+        else if(input.hasClass('number'))
+        {
+            var ph = /^[0-9]+(\.[0-9][0-9]?)?$/;
+            if(!ph.test(val))
+            {
+                msg = 'Invalid entry';
+                isValid = false;
+            }
+        }
+        else if(input.hasClass('phone'))
+        {
+            var ph = /^(234|0)[0-9]{7,10}$/;
+            if(!ph.test(val))
+            {
+                msg = 'Invalid entry';
+                isValid = false;
+            }
+        }
+        else if (input.hasClass('name'))
+        {
+            var filter = /^([a-zA-Z]+)([a-zA-Z\.\-\' ]*)([a-zA-Z]+)$/;
+            if(!filter.test(val))
+            {
+                msg = 'Invalid entry';
+                isValid = false;
+            }
+        }
+        else if(input.hasClass('match'))
+        {
+            var match = (input.attr('match'));
+            if($(match).val() !== val)
+            {
+                msg = 'Entries mismatch';
+                isValid = false;
+            }
+        }
+        else if(input.find("input[type=radio]").length>0 && input.find("input[type=radio]:checked").length==0)
+        {
+            msg = 'Required field';
             isValid = false;
         }
-    }
-    else if(input.hasClass('non-zero-integer'))
-    {
-        var test = /^[1-9]\d*$/;
-        if(!test.test(val))
-        {
-            msg = 'Invalid entry';
-            isValid = false;
-        }
-    }
-    else if(input.hasClass('non-zero-number'))
-    {
-        var test = /^(?=.*[1-9])\d+(\.\d+)?$/;
-        if(!test.test(val) && val !== "")
-        {
-            msg = 'Invalid entry';
-            isValid = false;
-        }
-    }
-    else if(input.hasClass('number'))
-    {
-        var ph = /^[0-9]+(\.[0-9][0-9]?)?$/;
-        if(!ph.test(val))
-        {
-            msg = 'Invalid entry';
-            isValid = false;
-        }
-    }
-    else if(input.hasClass('phone'))
-    {
-        var ph = /^(234|0)[0-9]{7,10}$/;
-        if(!ph.test(val))
-        {
-            msg = 'Invalid entry';
-            isValid = false;
-        }
-    }
-    else if (input.hasClass('name'))
-    {
-        var filter = /^([a-zA-Z]+)([a-zA-Z\.\-\' ]*)([a-zA-Z]+)$/;
-        if(!filter.test(val))
-        {
-            msg = 'Invalid entry';
-            isValid = false;
-        }
-
-    }
-    else if(input.hasClass('match'))
-    {
-        var match = (input.attr('match'));
-        if($(match).val() !== val)
-        {
-            msg = 'Entries mismatch';
-            isValid = false;
-        }
-    }
-    else if(input.find("input[type=radio]").length>0 && input.find("input[type=radio]:checked").length==0)
-    {
-        msg = 'Required field';
-        isValid = false;
     }
     if(input.hasClass('length') && isValid)
     {
