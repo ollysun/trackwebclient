0bbd44ff4f4d511e463dc4a464c95a5838ffc4e9
Olawale Lawal <lawalolawale@gmail.com>
2015-09-22 13:34:52 +0100

    CPTTK-247

--- web/js/hub_delivery.js
+++ web/js/hub_delivery.js
@@ -17,14 +17,14 @@ var parcels = {
 var Parcel_Destination = {
 
     Url: {
-        'allhubs' : '/hubs/allhubs',
-        'allecforhubs' : '/hubs/allecforhubs',
-        'staffdetails' : '/hubs/staffdetails',
+        'allhubs': '/hubs/allhubs',
+        'allecforhubs': '/hubs/allecforhubs',
+        'staffdetails': '/hubs/staffdetails',
         'generatemanifest': '/hubs/generatemanifest',
         'createbag': '/hubs/createbag'
     },
 
-    getNewStaffInfo: function() {
+    getNewStaffInfo: function () {
 
         return {
             id: '',
@@ -35,11 +35,11 @@ var Parcel_Destination = {
         }
     },
 
-    fillSelectOption: function(url, param, selectSelector, selectedValue) {
-        $.get( url, param, function(response){
-            if(response.status === 'success') {
+    fillSelectOption: function (url, param, selectSelector, selectedValue) {
+        $.get(url, param, function (response) {
+            if (response.status === 'success') {
                 var html = '<option value="">Select Name...</option>';
-                $.each(response.data, function(i, item) {
+                $.each(response.data, function (i, item) {
                     selected = (selectedValue == item.id) ? 'selected="selected"' : '';
                     html += "<option value='" + item.id + "'" + selected + ">" + item.name.toUpperCase() + "</option>";
                 });
@@ -48,10 +48,10 @@ var Parcel_Destination = {
         });
     },
 
-    searchStaffDetails: function(code) {
+    searchStaffDetails: function (code) {
         var self = this;
-        $.get( Parcel_Destination.Url.staffdetails, { code: code }, function(response){
-            if(response.status === 'success') {
+        $.get(Parcel_Destination.Url.staffdetails, {code: code}, function (response) {
+            if (response.status === 'success') {
                 var staff = self.getNewStaffInfo();
                 staff.id = response.data.id;
                 staff.code = code;
@@ -70,7 +70,7 @@ var Parcel_Destination = {
         });
     },
 
-    updateStaffDetails: function(staff) {
+    updateStaffDetails: function (staff) {
 
         parcels.staff_id = staff.id;
         parcels.staff_code = staff.code;
@@ -80,7 +80,7 @@ var Parcel_Destination = {
         $('#staff_role').html(staff.staff_role);
     },
 
-    moveToInTransit: function(parcels) {
+    moveToInTransit: function (parcels) {
 
 
         $.ajax({
@@ -88,23 +88,23 @@ var Parcel_Destination = {
             type: 'POST',
             dataType: 'JSON',
             data: JSON.stringify(parcels),
-            success: function(response) {
-                if(response.status == 'success') {
+            success: function (response) {
+                if (response.status == 'success') {
                     alert('Manifest Generated Successfully!');
                 } else {
                     alert('An error occurred when generating manifest. Please try again later');
                 }
             },
-            error: function(err) {
+            error: function (err) {
                 alert('An error occurred when generating manifest. Please try again later');
             },
-            complete: function(jqXHR) {
+            complete: function (jqXHR) {
 
             }
         })
     },
 
-    createBag: function(parcels) {
+    createBag: function (parcels) {
 
 
         $.ajax({
@@ -112,17 +112,17 @@ var Parcel_Destination = {
             type: 'POST',
             dataType: 'JSON',
             data: JSON.stringify(parcels),
-            success: function(response) {
-                if(response.status == 'success') {
+            success: function (response) {
+                if (response.status == 'success') {
                     alert('Bag with id ' + response.data.id + ' has been created successfully!');
                 } else {
                     alert('An error occurred while trying to create bag. #' + response.message);
                 }
             },
-            error: function(err) {
+            error: function (err) {
                 alert('We are unable to process this request. Please try again later.');
             },
-            complete: function(jqXHR) {
+            complete: function (jqXHR) {
 
             }
         })
@@ -136,66 +136,66 @@ var TableHelper = {
 
 
         var trs = $(tableSelector + ' tbody').children();
-        if(!this.isDefined(colIndex)) {
+        if (!this.isDefined(colIndex)) {
             colIndex = 0;
         }
-        for(var i = 0; i < trs.length; i++) {
-            var children = $(trs[i]).find('td:eq(' + colIndex + ')').first().html(i+1);
+        for (var i = 0; i < trs.length; i++) {
+            var children = $(trs[i]).find('td:eq(' + colIndex + ')').first().html(i + 1);
         }
     },
 
-    isItemInTableColumn: function(tableSelector, colIndex, searchItem) {
+    isItemInTableColumn: function (tableSelector, colIndex, searchItem) {
 
         var tempText = '';
         var found = false;
         var trs = $(tableSelector + ' tbody').children();
-        if(!this.isDefined(colIndex)) {
+        if (!this.isDefined(colIndex)) {
             colIndex = 0;
         }
 
-        $.each(trs, function(index, tr){
+        $.each(trs, function (index, tr) {
             tempText = $.trim($(tr).children().eq(colIndex).html());
-            if((tempText) == searchItem) {
+            if ((tempText) == searchItem) {
                 found = true;
             }
         });
         return found;
     },
 
-    getColumTextData: function(tableSelector, colIndex) {
+    getColumTextData: function (tableSelector, colIndex) {
 
         var items = [];
         var trs = $(tableSelector + ' tbody').children();
-        if(!this.isDefined(colIndex)) {
+        if (!this.isDefined(colIndex)) {
             colIndex = 0;
         }
 
-        $.each(trs, function(index, tr){
+        $.each(trs, function (index, tr) {
             items.push($.trim($(tr).children().eq(colIndex).html()));
         });
         return items;
     },
 
-    setCellData: function(tableSelector, rowIndex, colIndex, content) {
+    setCellData: function (tableSelector, rowIndex, colIndex, content) {
 
-        if(!this.isDefined(rowIndex) || !this.isDefined(colIndex)) {
+        if (!this.isDefined(rowIndex) || !this.isDefined(colIndex)) {
             return;
         }
         var trs = $(tableSelector + ' tbody').children();
         $(trs).eq(rowIndex).children().eq(colIndex).html(content);
     },
 
-    setColumnData: function(tableSelector, colIndex, content, withCheckbox) {
+    setColumnData: function (tableSelector, colIndex, content, withCheckbox) {
 
-        if(!this.isDefined(colIndex)) {
+        if (!this.isDefined(colIndex)) {
             return;
         }
         var trs = $(tableSelector + ' tbody').children();
 
-        $.each(trs, function(rowIndex, tr){
-            if(withCheckbox) {
+        $.each(trs, function (rowIndex, tr) {
+            if (withCheckbox) {
                 var chkValue = $(tr).children().find(':checkbox').attr('value');
-                if(chkValue) {
+                if (chkValue) {
                     $(tr).children().eq(colIndex).html(content);
                 }
             } else {
@@ -204,15 +204,15 @@ var TableHelper = {
         });
     },
 
-    getCellData: function(tableSelector, colIndex, rowIndex) {
+    getCellData: function (tableSelector, colIndex, rowIndex) {
         var cellData = '';
         var trs = $(tableSelector + ' tbody').children();
-        if(!this.isDefined(colIndex)) {
+        if (!this.isDefined(colIndex)) {
             colIndex = 0;
         }
 
-        $.each(trs, function(index, tr){
-            if(index === rowIndex) {
+        $.each(trs, function (index, tr) {
+            if (index === rowIndex) {
                 cellData = $.trim($(tr).children().eq(colIndex).html());
             }
         });
@@ -220,7 +220,7 @@ var TableHelper = {
         return cellData;
     },
 
-    isCallback: function(callback) {
+    isCallback: function (callback) {
         return (callback && typeof(callback) === typeof(Function));
     },
 
@@ -230,7 +230,7 @@ var TableHelper = {
     }
 };
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('#staff_info').hide();
     $('#btnGenerate').attr('disabled', true);
@@ -239,7 +239,7 @@ $(document).ready(function(){
     var bid = $('#branch_name').attr('data-bid');
     fillBranchesOrHub(btype, bid);
 
-    $('#branch_type').on('change', function(){
+    $('#branch_type').on('change', function () {
         var type = $(this).val();
         fillBranchesOrHub(type);
     });
@@ -256,21 +256,30 @@ $(document).ready(function(){
         Parcel_Destination.fillSelectOption(url, {}, '#branch_name', bid);
     }
 
-    $('#manifest').on('click', function(event) {
+    $('#manifest').on('click', function (event) {
 
+        validateParcels();
+        populateDialog(parcels, '#genManifest');
+
+        $('#staff_info').hide();
+        $('#btnGenerate').attr('disabled', true);
+        $('#genManifest').modal('show');
+    });
+
+    function validateParcels() {
         var chkboxes = $('.chk_next');
         var selected = false;
         var same_branch = true;
         parcels.waybills = [];
         var old_branch = '';
-        $.each(chkboxes, function(i, chk){
+        $.each(chkboxes, function (i, chk) {
 
             var checked = $(chk).is(':checked');
-            if(checked) {
+            if (checked) {
                 selected = true;
                 var waybill = {};
                 var tr = $(chk).closest('tr');
-                if(!old_branch) {
+                if (!old_branch) {
                     old_branch = parcels.to_branch_id = $(tr).attr('data-to-branch-id');
                 }
                 waybill.number = $(tr).attr('data-waybill');
@@ -279,42 +288,37 @@ $(document).ready(function(){
                 parcels.to_branch_id = $(tr).attr('data-to-branch-id');
                 parcels.to_branch_name = TableHelper.getCellData('#next_dest', 4, $(tr).index());
 
-                if(old_branch !== parcels.to_branch_id) {
+                if (old_branch !== parcels.to_branch_id) {
                     same_branch = false;
                 }
             }
         });
 
-        if(!selected) {
+        if (!selected) {
             alert('You must select at least one parcel!');
             event.preventDefault();
             return;
         }
 
-        if(!same_branch) {
+        if (!same_branch) {
             alert('Manifest can only be generated for same next destination branch!');
             event.preventDefault();
             return;
         }
+    }
 
-        populateDialog(parcels);
-        $('#staff_info').hide();
-        $('#btnGenerate').attr('disabled', true);
-        $('#genManifest').modal('show');
-    });
+    function populateDialog(parcels, targetModal) {
 
-    function populateDialog(parcels) {
-
-        $('#bag_dlg_location').val(parcels.to_branch_name);
+        $(targetModal+' #dlg_location').val(parcels.to_branch_name);
         var html = '';
-        $.each(parcels.waybills, function(i, waybill){
+        $.each(parcels.waybills, function (i, waybill) {
             html += "<tr>";
-            html += "<td>" + (i+1) + "</td>";
+            html += "<td>" + (i + 1) + "</td>";
             html += "<td>" + waybill.number + "</td>";
             html += "<td>" + waybill.final + "</td>";
             html += "</tr>";
         });
-        $('#tbl_manifest > tbody').html(html);
+        $(targetModal+' #tbl_manifest > tbody').html(html);
     }
 
     $('#staff').on('keypress', function (event) {
@@ -323,44 +327,44 @@ $(document).ready(function(){
 
             event.preventDefault();
             var staff_code = $(this).val();
-            if(staff_code == '') {
+            if (staff_code == '') {
                 return;
             }
             Parcel_Destination.searchStaffDetails(staff_code);
         }
     });
 
-    $('#btn_staff').on('click', function(event){
+    $('#btn_staff').on('click', function (event) {
         var staff_code = $('#staff').val();
-        if(staff_code == '') {
+        if (staff_code == '') {
             return;
         }
         Parcel_Destination.searchStaffDetails(staff_code);
         event.preventDefault();
     });
 
-    $('#btnGenerate').on('click', function(event){
+    $('#btnGenerate').on('click', function (event) {
 
         $('#payload').val(JSON.stringify(parcels));
     });
 
-    $('#branch_name').on('change', function(){
+    $('#branch_name').on('change', function () {
         var name = $(this).val();
-        if(name !== '') {
+        if (name !== '') {
             name = $(this).find('option:selected').text();
         }
         TableHelper.setColumnData('#next_dest', 4, name, true);
     });
 
-    $('#chk_all').on('click', function(event){
+    $('#chk_all').on('click', function (event) {
         var is_checked = $(this).is(':checked');
         var checkboxes = $('#next_dest tr input:checkbox');
-        if(is_checked) {
-            $.each(checkboxes, function(i, chk){
+        if (is_checked) {
+            $.each(checkboxes, function (i, chk) {
                 $(chk).prop('checked', true);
             });
         } else {
-            $.each(checkboxes, function(i, chk){
+            $.each(checkboxes, function (i, chk) {
                 $(chk).prop('checked', false);
             });
         }
@@ -370,50 +374,14 @@ $(document).ready(function(){
         $("form#records_filter").submit();
     });
 
-    $('#btnCreateBag').on('click', function(event){
-        var chkboxes = $('.chk_next');
-        var selected = false;
-        var same_branch = true;
-        parcels.waybills = [];
-        var old_branch = '';
-        $.each(chkboxes, function(i, chk){
+    $('#btnCreateBag').on('click', function (event) {
 
-            var checked = $(chk).is(':checked');
-            if(checked) {
-                selected = true;
-                var waybill = {};
-                var tr = $(chk).closest('tr');
-                if(!old_branch) {
-                    old_branch = parcels.to_branch_id = $(tr).attr('data-to-branch-id');
-                }
-                waybill.number = $(tr).attr('data-waybill');
-                waybill.final = TableHelper.getCellData('#next_dest', 5, $(tr).index());
-                parcels.waybills.push(waybill);
-                parcels.to_branch_id = $(tr).attr('data-to-branch-id');
-                parcels.to_branch_name = TableHelper.getCellData('#next_dest', 4, $(tr).index());
-
-                if(old_branch !== parcels.to_branch_id) {
-                    same_branch = false;
-                }
-            }
-        });
-
-        if(!selected) {
-            alert('You must select at least one parcel!');
-            event.preventDefault();
-            return;
-        }
-
-        if(!same_branch) {
-            alert('Manifest can only be generated for same next destination branch!');
-            event.preventDefault();
-            return;
-        }
-        populateDialog(parcels);
+        validateParcels();
+        populateDialog(parcels,'#createBag');
         $('#createBag').modal('show');
     });
 
-    $('#btnBag').on('click', function(event){
+    $('#btnBag').on('click', function (event) {
         event.preventDefault();
         Parcel_Destination.createBag(parcels);
     });
