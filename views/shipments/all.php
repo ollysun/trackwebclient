cbbce3cdfdd1da0b0b8959869ef402b7295be253
Olawale Lawal <lawalolawale@gmail.com>
2015-09-23 18:07:56 +0100

    Included the originating center details

--- views/shipments/all.php
+++ views/shipments/all.php
@@ -93,6 +93,7 @@ if($search){
                     <th>Created Date</th>
                     <th>Pieces</th>
                     <th>Status</th>
+                    <th>Originating Centre</th>
                     <th>Action</th>
                 </tr>
                 </thead>
@@ -112,6 +113,7 @@ if($search){
                             <td><?= date(ServiceConstant::DATE_TIME_FORMAT,strtotime($parcel['created_date'])); ?></td>
                             <td><?= $parcel['no_of_package']; ?></td>
                             <td><?= ServiceConstant::getStatus($parcel['status']); ?></td>
+                            <td><?= strtoupper($parcel['created_branch']['name']); ?></td>
                             <td><a href="<?= Url::to(['site/viewwaybill?id='.$parcel['id']]) ?>" class="btn btn-xs btn-default"><i class="fa fa-eye">&nbsp;</i> View</a></td>
                         </tr>
                     <?php
