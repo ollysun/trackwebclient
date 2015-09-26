3da358071d643fcb342f7007cd1671e92d6166a7
Adegoke Obasa <goke@cottacush.com>
2015-09-26 19:19:07 +0100

    Create reset password view

--- views/site/forgotpassword.php
+++ views/site/forgotpassword.php
@@ -7,9 +7,7 @@ use Adapter\Util\Calypso;
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\LoginForm */
 
-$this->title = 'Change Password';
-$data = Calypso::getInstance()->getPageData();
-
+$this->title = 'Reset Password';
 ?>
 <div class="l-sigin-page">
 
@@ -17,23 +15,14 @@ $data = Calypso::getInstance()->getPageData();
 
         <?php echo Calypso::showFlashMessages(); ?>
         <div class="card-header">
-            <h3 class="card-title">Change Password</h3>
+            <h3 class="card-title">Reset Password</h3>
         </div>
-        <form action="changepassword" method="post" enctype="multipart/form-data">
+        <form action="" method="post" enctype="multipart/form-data">
             <div class="form-group">
-                <label for="old_password">Old Password</label>
+                <label for="old_password">Email Address</label>
                 <input id="old_password" name="old_password" type="password" class="form-control" />
             </div>
-            <div class="form-group">
-                <label for="new_password">New Password</label>
-                <input id="new_password" name="new_password" type="password" class="form-control" />
-            </div>
-            <div class="form-group">
-                <label for="password">Confirm Password</label>
-                <input id="password" name="password" type="password" class="form-control" />
-            </div>
-            <input type="hidden" name="task" value="change">
-            <button type="submit" class="btn btn-primary btn-block">Change Password</button>
+            <button type="submit" class="btn btn-primary btn-block">Reset Password</button>
         </form>
     </div>
     <p class="text-center"><a class="forgot-link" href="<?= \yii\helpers\Url::base(true);?>">&larr; Go back</a></p>
@@ -42,3 +31,4 @@ $data = Calypso::getInstance()->getPageData();
         <p id="footer-copyright" class="col-xs-12">&copy; 2015<?php if(date('Y') > 2015): echo " &ndash; ".date('Y'); endif; ?> CourierPlus. All Rights Reserved.</p>
     </div>
 </div>
+
