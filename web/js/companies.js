1895cc1a5d263b9dd9a78e49d0ac23acc72d842b
Adegoke Obasa <goke@cottacush.com>
2015-09-28 13:03:12 +0100

    Change client_id from admin_id to user_auth_id

--- web/js/companies.js
+++ web/js/companies.js
@@ -1,18 +1,20 @@
 (function () {
-    $(document).ready(function() {
+    $(document).ready(function () {
         $("#state").change(function () {
-            $("#city").html("<option>Loading...</option>");
-            $.getJSON("/admin/cities?state_id=" + $(this).val(), function(data){
-                var html = "";
-                console.log(data.data);
-                $(data.data).each(function (i, v) {
-                    var option = new Option(v.name.toUpperCase(), v.id);
-                    html += option.outerHTML;
-                });
-                $("#city").html(html);
-            }).error(function() {
+            if ($(this).val() != "") {
+                $("#city").html("<option>Loading...</option>");
+                $.getJSON("/admin/cities?state_id=" + $(this).val(), function (data) {
+                    var html = "";
+                    console.log(data.data);
+                    $(data.data).each(function (i, v) {
+                        var option = new Option(v.name.toUpperCase(), v.id);
+                        html += option.outerHTML;
+                    });
+                    $("#city").html(html);
+                }).error(function () {
 
-            });
+                });
+            }
         });
     });
 })();
