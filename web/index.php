7096f223fcdb0b1638805d2bf39966b5cbce94e1
Adeyemi Olaoye <yemi@cottacush.com>
2015-09-29 13:03:24 +0100

    fixed issue with debug mode

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 session_start();
 // comment out the following two lines when deployed to production
 
-if (in_array(getenv("APPLICATION_ENV"), ["production", "staging"])) {
+if (!in_array(getenv("APPLICATION_ENV"), ["production", "staging"])) {
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YII_ENV') or define('YII_ENV', 'dev');
 }
