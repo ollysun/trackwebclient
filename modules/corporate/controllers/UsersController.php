a205f0d361a187f065e02f5e6b10852d2fe7717e
Adegoke Obasa <goke@cottacush.com>
2015-10-02 12:24:33 +0100

    Add company routes to permissions map

--- modules/corporate/controllers/UsersController.php
+++ modules/corporate/controllers/UsersController.php
@@ -6,9 +6,12 @@
 namespace app\modules\corporate\controllers;
 
 
+use Adapter\CompanyAdapter;
+use Adapter\Util\Calypso;
+use app\controllers\BaseController;
 use yii\web\Controller;
 
-class UsersController extends Controller
+class UsersController extends BaseController
 {
 
     /**
@@ -18,6 +21,32 @@ class UsersController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $companyAdapter = new CompanyAdapter();
+        $companyId = Calypso::getValue(Calypso::getInstance()->session("user_session"), 'company_id');
+
+        $filters = [];
+
+        // Add Offset and Count
+        $page = \Yii::$app->getRequest()->get('page', 1);
+        $offset = ($page - 1) * $this->page_width;
+        $filters['offset'] = $offset;
+        $filters['count'] = $this->page_width;
+
+        $query = \Yii::$app->getRequest()->get('search');
+        if(!is_null($query)) {
+//            $filters = ['name' => $query];
+            $page = 1; // Reset page
+        }
+
+        $usersData = $companyAdapter->getUsers($filters);
+        $users = Calypso::getValue($usersData, 'users', []);
+        $totalCount = Calypso::getValue($usersData, 'total_count', 0);
+
+        return $this->render('index', [
+            'users' => $users,
+            'offset' => $offset,
+            'total_count' => $totalCount,
+            'page_width' => $this->page_width
+        ]);
     }
 }
