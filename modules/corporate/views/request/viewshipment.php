8f377b6b132f637f5ffca85f7e1281d6efe1a099
Adegoke Obasa <goke@cottacush.com>
2015-10-12 16:30:14 +0100

    Create view for pickup request

--- modules/corporate/views/request/viewshipment.php
+++ modules/corporate/views/request/viewshipment.php
@@ -1,3 +1,14 @@
+<?php
+use Adapter\Util\Calypso;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use Adapter\Globals\ServiceConstant;
+
+/* @var $this yii\web\View */
+$this->title = 'View Shipment Request ';
+$this->params['page_title'] = 'Shipment Request';
+$this->params['breadcrumbs'][] = 'Shipment Request';
+?>
 <div class="main-box no-header">
     <div class="main-box-body row">
         <div class="col-xs-12 col-sm-6">
@@ -6,46 +17,55 @@
                 <div class="row form-group">
                     <div class="col-xs-6">
                         <label>Firstname</label>
+
                         <div class="form-control-static">Tinuke</div>
                     </div>
                     <div class="col-xs-6">
                         <label>Lastname</label>
+
                         <div class="form-control-static">Olotu</div>
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-xs-6">
                         <label for="">Email address</label>
+
                         <div class="form-control-static">ladapo5704@gmail.com</div>
                     </div>
                     <div class="col-xs-6">
                         <label>Phone number</label>
+
                         <div class="form-control-static">0803456788</div>
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-xs-6">
                         <label for="">Company</label>
-                        <div class="form-control-static">ladapo5704@gmail.com</div>
+
+                        <div class="form-control-static">Company</div>
                     </div>
                     <div class="col-xs-6">
                         <label>Address</label>
-                        <div class="form-control-static">0803456788</div>
+
+                        <div class="form-control-static">No 5b Chris Ali</div>
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-xs-6">
                         <label>State</label>
+
                         <div class="form-control-static">Oyo</div>
                     </div>
                     <div class="col-xs-6">
                         <label>City</label>
+
                         <div class="form-control-static">Ibadan</div>
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-xs-6">
                         <label for="">Country</label>
+
                         <div class="form-control-static">Nigeria</div>
                     </div>
                 </div>
@@ -58,20 +78,24 @@
                 <div class="row form-group">
                     <div class="col-xs-6">
                         <label>Estimated Weight (Kg)</label>
+
                         <div class="form-control-static">10 Kg</div>
                     </div>
                     <div class="col-xs-6">
                         <label for="">No of packages</label>
+
                         <div class="form-control-static">200</div>
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-xs-6">
                         <label>Shipment Value</label>
+
                         <div class="form-control-static">NGN 5000</div>
                     </div>
                     <div class="col-xs-6">
                         <label>Parcel Description</label>
+
                         <div class="form-control-static">
                             Lorem ipsum
                         </div>
@@ -89,9 +113,12 @@
                         <div class="row form-group">
                             <div class="col-xs-6">
                                 <label>Cash on Delivery</label>
-                                <div class="form-control-static">Documents</div>								</div>
+
+                                <div class="form-control-static">Documents</div>
+                            </div>
                             <div class="col-xs-6">
                                 <label>Order Number</label>
+
                                 <div class="form-control-static">
                                     Kg
                                 </div>
@@ -107,16 +134,19 @@
                 <legend>Creation Information</legend>
                 <div class="form-group">
                     <label>Company</label>
+
                     <div class="form-control-static">
                         Adio and Sons
                     </div>
                 </div>
                 <div class="form-group">
                     <label>Created By</label>
+
                     <div class="form-control-static">Ajanlekoko Ajala</div>
                 </div>
                 <div class="form-group">
                     <label>Date &amp; Time</label>
+
                     <div class="form-control-static">19 Sep 2015 23:45</div>
                 </div>
             </fieldset>
