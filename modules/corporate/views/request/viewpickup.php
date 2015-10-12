72bb1fd9aae36b6d48642d93b917081792bdb12a
Adegoke Obasa <goke@cottacush.com>
2015-10-12 16:30:14 +0100

    Create view for pickup request

--- modules/corporate/views/request/viewpickup.php
+++ modules/corporate/views/request/viewpickup.php
@@ -1,4 +1,135 @@
 <?php
-/**
- * @author Adegoke Obasa <goke@cottacush.com>
- */
+use Adapter\Util\Calypso;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use Adapter\Globals\ServiceConstant;
+
+/* @var $this yii\web\View */
+$this->title = 'View Shipment Request ';
+$this->params['page_title'] = 'Shipment Request';
+$this->params['breadcrumbs'][] = 'Shipment Request';
+?>
+<div class="main-box no-header">
+    <div class="main-box-body row">
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Pickup Detail</legend>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>Contact Name</label>
+
+                        <div class="form-control-static">Tinuke</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>Contact Phone number</label>
+
+                        <div class="form-control-static">0803456788</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>State</label>
+
+                        <div class="form-control-static">Oyo</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>City</label>
+
+                        <div class="form-control-static">Ibadan</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label for="">Address</label>
+
+                        <div class="form-control-static">Nigeria</div>
+                    </div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Destination Detail</legend>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>Contact Name</label>
+
+                        <div class="form-control-static">Tinuke</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>Contact Phone number</label>
+
+                        <div class="form-control-static">0803456788</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>State</label>
+
+                        <div class="form-control-static">Oyo</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>City</label>
+
+                        <div class="form-control-static">Ibadan</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label for="">Address</label>
+
+                        <div class="form-control-static">Nigeria</div>
+                    </div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Shipment Details</legend>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>Request Details</label>
+
+                        <div class="form-control-static">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>Shipment Description</label>
+
+                        <div class="form-control-static">
+                            Lorem ipsum
+                        </div>
+
+                    </div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Creation Information</legend>
+                <div class="form-group">
+                    <label>Company</label>
+
+                    <div class="form-control-static">
+                        Adio and Sons
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Created By</label>
+
+                    <div class="form-control-static">Ajanlekoko Ajala</div>
+                </div>
+                <div class="form-group">
+                    <label>Date &amp; Time</label>
+
+                    <div class="form-control-static">19 Sep 2015 23:45</div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+    </div>
+</div>
