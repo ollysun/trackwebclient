fc74a1ff333f13de138b0ce8c217b18a333ea896
Adegoke Obasa <goke@cottacush.com>
2015-10-12 10:48:02 +0100

    Create view for pending pickup requests

--- modules/corporate/controllers/PendingController.php
+++ modules/corporate/controllers/PendingController.php
@@ -13,7 +13,7 @@ use app\controllers\BaseController;
 class PendingController extends BaseController
 {
     /**
-     *
+     * Pending Shipment Requests Action
      * @author Adegoke Obasa <goke@cottacush.com>
      */
     public function actionShipments()
@@ -51,10 +51,41 @@ class PendingController extends BaseController
     }
 
     /**
+     * Pending Pickup Requests action
      * @author Adegoke Obasa <goke@cottacush.com>
      */
     public function actionPickups()
     {
+        $companyAdapter = new CompanyAdapter();
 
+        $filters = [
+            'status' => CompanyAdapter::STATUS_PENDING
+        ];
+
+        // Add Offset and Count
+        $page = \Yii::$app->getRequest()->get('page', 1);
+
+        $query = \Yii::$app->getRequest()->get('search');
+        if(!is_null($query)) {
+            $filters['waybill_number'] = $query;
+            $page = 1; // Reset page
+        }
+
+        $offset = ($page - 1) * $this->page_width;
+        $filters['offset'] = $offset;
+        $filters['count'] = $this->page_width;
+
+        $requestsData = $companyAdapter->getPickupRequests($filters);
+
+
+        $requests = Calypso::getValue($requestsData, 'requests', []);
+        $totalCount = Calypso::getValue($requestsData, 'total_count', 0);
+
+        return $this->render('pickup', [
+            'requests' => $requests,
+            'offset' => $offset,
+            'page_width' => $this->page_width,
+            'total_count' => $totalCount
+        ]);
     }
 }
