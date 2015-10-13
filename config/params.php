ff1883f91f7bd2a8a5ba1d8f9792ede60e4e134a
Adegoke Obasa <goke@cottacush.com>
2015-10-13 14:07:23 +0100

    Loading of States and Cities - Change service base url

--- config/params.php
+++ config/params.php
@@ -2,7 +2,6 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-//    'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://local.courierplus.tntservice.com/",
-    'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://staging-tnt-service.cottacush.com/",
+    'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://local.courierplus.tntservice.com/",
     'cacheAppPrefix' => 'track_plus::'
 ];
