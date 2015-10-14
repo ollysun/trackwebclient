d3a256d67f6e6b646c6e3c01f720a0db05a9d0cf
Adegoke Obasa <goke@cottacush.com>
2015-10-14 14:58:28 +0100

    VIew Company - Create company detail view

---
+++ views/admin/viewcompany.php
@@ -0,0 +1,121 @@
+<div class="main-box no-header">
+    <div class="main-box-body row">
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Company Detail</legend>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>Contact Name</label>
+
+                        <div class="form-control-static">Adegoke Obasa</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>Contact Phone number</label>
+
+                        <div class="form-control-static">08079228385</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>State</label>
+
+                        <div class="form-control-static">ABIA</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>City</label>
+
+                        <div class="form-control-static">ABA</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label for="">Address</label>
+
+                        <div class="form-control-static">Adeola</div>
+                    </div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Primary Contact</legend>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>Contact Name</label>
+
+                        <div class="form-control-static">Adenike Obasa</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>Contact Phone number</label>
+
+                        <div class="form-control-static">08079228385</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>State</label>
+
+                        <div class="form-control-static">ABIA</div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>City</label>
+
+                        <div class="form-control-static">ABA</div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label for="">Address</label>
+
+                        <div class="form-control-static">Odeku</div>
+                    </div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Shipment Details</legend>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>Request Details</label>
+
+                        <div class="form-control-static">
+                            Details Here                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>Shipment Description</label>
+
+                        <div class="form-control-static">
+                            A New Parcel                        </div>
+
+                    </div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <fieldset>
+                <legend>Creation Information</legend>
+                <div class="form-group">
+                    <label>Company</label>
+
+                    <div class="form-control-static">
+                        ADELABU INDUSTRIES                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Created By</label>
+
+                    <div class="form-control-static">Adegoke Obasa</div>
+                </div>
+                <div class="form-group">
+                    <label>Date &amp; Time</label>
+
+                    <div class="form-control-static"></div>
+                </div>
+            </fieldset>
+            <br><br>
+        </div>
+    </div>
+</div>
