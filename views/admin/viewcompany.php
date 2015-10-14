0aa8f7121909dd6eae8a7c759df5e23d14ec3e34
Adegoke Obasa <goke@cottacush.com>
2015-10-14 15:35:34 +0100

    VIew Company - Render company details

--- views/admin/viewcompany.php
+++ views/admin/viewcompany.php
@@ -1,3 +1,15 @@
+<?php
+use Adapter\Util\Calypso;
+use Adapter\Util\Util;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use Adapter\Globals\ServiceConstant;
+
+/* @var $this yii\web\View */
+$this->title = 'Company Details';
+$this->params['page_title'] = strtoupper(Calypso::getValue($company, 'name', ''));
+$this->params['breadcrumbs'][] = 'Company Details';
+?>
 <div class="main-box no-header">
     <div class="main-box-body row">
         <div class="col-xs-12 col-sm-6">
@@ -5,33 +17,46 @@
                 <legend>Company Detail</legend>
                 <div class="row form-group">
                     <div class="col-xs-6">
-                        <label>Contact Name</label>
+                        <label>Name</label>
 
-                        <div class="form-control-static">Adegoke Obasa</div>
+                        <div
+                            class="form-control-static"><?= strtoupper(Calypso::getValue($company, 'name', '')); ?></div>
                     </div>
                     <div class="col-xs-6">
-                        <label>Contact Phone number</label>
+                        <label>Reg No</label>
 
-                        <div class="form-control-static">08079228385</div>
+                        <div class="form-control-static"><?= Calypso::getValue($company, 'reg_no', 'N/A'); ?></div>
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-xs-6">
-                        <label>State</label>
+                        <label>Email Address</label>
 
-                        <div class="form-control-static">ABIA</div>
+                        <div class="form-control-static"><?= Calypso::getValue($company, 'email', ''); ?></div>
                     </div>
+                    <div class="col-xs-6">
+                        <label>Phone number</label>
+
+                        <div class="form-control-static"><?= Calypso::getValue($company, 'phone_number', ''); ?></div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-12">
+                        <label for="">Address</label>
+
+                        <div class="form-control-static"><?= Calypso::getValue($company, 'address', ''); ?></div>
+                    </div>
+                </div>
+                <div class="row form-group">
                     <div class="col-xs-6">
                         <label>City</label>
 
-                        <div class="form-control-static">ABA</div>
+                        <div class="form-control-static"><?= strtoupper(Calypso::getValue($company, 'city.name', '')); ?></div>
                     </div>
-                </div>
-                <div class="row form-group">
                     <div class="col-xs-6">
-                        <label for="">Address</label>
+                        <label>State</label>
 
-                        <div class="form-control-static">Adeola</div>
+                        <div class="form-control-static"><?= strtoupper(Calypso::getValue($company, 'state.name', '')); ?></div>
                     </div>
                 </div>
             </fieldset>
@@ -42,33 +67,25 @@
                 <legend>Primary Contact</legend>
                 <div class="row form-group">
                     <div class="col-xs-6">
-                        <label>Contact Name</label>
+                        <label>Name</label>
 
-                        <div class="form-control-static">Adenike Obasa</div>
+                        <div
+                            class="form-control-static"><?= ucwords(Calypso::getValue($company, 'primary_contact.firstname', '')); ?>
+                            <?= ucwords(Calypso::getValue($company, 'primary_contact.lastname', '')); ?></div>
                     </div>
                     <div class="col-xs-6">
-                        <label>Contact Phone number</label>
+                        <label>Email Address</label>
 
-                        <div class="form-control-static">08079228385</div>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-xs-6">
-                        <label>State</label>
-
-                        <div class="form-control-static">ABIA</div>
-                    </div>
-                    <div class="col-xs-6">
-                        <label>City</label>
-
-                        <div class="form-control-static">ABA</div>
+                        <div
+                            class="form-control-static"><?= ucwords(Calypso::getValue($company, 'primary_contact_auth.email', '')); ?></div>
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-xs-6">
-                        <label for="">Address</label>
+                        <label>Phone Number</label>
 
-                        <div class="form-control-static">Odeku</div>
+                        <div
+                            class="form-control-static"><?= Calypso::getValue($company, 'primary_contact.phone_number', ''); ?></div>
                     </div>
                 </div>
             </fieldset>
@@ -76,20 +93,33 @@
         </div>
         <div class="col-xs-12 col-sm-6">
             <fieldset>
-                <legend>Shipment Details</legend>
+                <legend>Relations Officer</legend>
                 <div class="row form-group">
                     <div class="col-xs-6">
-                        <label>Request Details</label>
+                        <label>Name</label>
 
-                        <div class="form-control-static">
-                            Details Here                        </div>
+                        <div
+                            class="form-control-static"><?= ucwords(Calypso::getValue($company, 'relations_officer.fullname', '')); ?></div>
                     </div>
                     <div class="col-xs-6">
-                        <label>Shipment Description</label>
+                        <label>Staff ID</label>
 
-                        <div class="form-control-static">
-                            A New Parcel                        </div>
+                        <div
+                            class="form-control-static"><?= ucwords(Calypso::getValue($company, 'relations_officer.staff_id', '')); ?></div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <label>Email</label>
 
+                        <div
+                            class="form-control-static"><?= ucwords(Calypso::getValue($company, 'relations_officer_auth.email', '')); ?></div>
+                    </div>
+                    <div class="col-xs-6">
+                        <label>Phone Number</label>
+
+                        <div
+                            class="form-control-static"><?= ucwords(Calypso::getValue($company, 'relations_officer.phone', '')); ?></div>
                     </div>
                 </div>
             </fieldset>
@@ -99,20 +129,10 @@
             <fieldset>
                 <legend>Creation Information</legend>
                 <div class="form-group">
-                    <label>Company</label>
-
-                    <div class="form-control-static">
-                        ADELABU INDUSTRIES                    </div>
-                </div>
-                <div class="form-group">
-                    <label>Created By</label>
-
-                    <div class="form-control-static">Adegoke Obasa</div>
-                </div>
-                <div class="form-group">
                     <label>Date &amp; Time</label>
 
-                    <div class="form-control-static"></div>
+                    <div
+                        class="form-control-static"><?= Util::convertDateTimeToTime(Calypso::getValue($company, 'created_date', '')); ?></div>
                 </div>
             </fieldset>
             <br><br>
