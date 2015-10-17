2cd48e119370d086fd7fe545cbc9f677b1e7bb06
Adegoke Obasa <goke@cottacush.com>
2015-10-17 08:49:20 +0100

    Fix short tag in pickup and shipment views

--- modules/corporate/views/request/shipment.php
+++ modules/corporate/views/request/shipment.php
@@ -95,7 +95,7 @@ $this->params['content_header_button'] = '<button type="button" class="btn btn-p
                                        href="<?= Url::toRoute(['/corporate/request/viewshipment', 'id' => Calypso::getValue($request, 'id')]) ?>"
                                        class="btn btn-xs btn-default"><i class="fa fa-eye"></i></a>
 
-                                    <?if(Calypso::getValue($request, 'status') == \Adapter\CompanyAdapter::STATUS_PENDING):?>
+                                    <?php if(Calypso::getValue($request, 'status') == \Adapter\CompanyAdapter::STATUS_PENDING):?>
                                         <form method="post" action="<?= Url::to('/corporate/request/cancelshipment'); ?>">
                                             <input type="hidden" name="request_id" value="<?= Calypso::getValue($request, 'id');?>" />
                                             <button type="submit" title="Cancel this request"
