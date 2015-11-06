874779e4f3611a4053102a010ef4a5554820184b
Adegoke Obasa <goke@cottacush.com>
2015-11-06 13:15:43 +0100

    Add var before variables in js

--- web/js/city.js
+++ web/js/city.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("button[data-target='#editModal']").on('click', function (event) {
-        target = $(this).attr('data-target');
-        _id = $(this).attr('data-id');
+        var target = $(this).attr('data-target');
+        var _id = $(this).attr('data-id');
         $(target+" input[name='city_name']").val($("td[class='n"+_id+"']").text());
         $(target+" input[name='transit_time']").val($("td[class='t"+_id+"']").text());
         $(target+" select[name='state']").val($(this).attr('data-state-id'));
