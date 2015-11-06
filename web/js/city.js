c30daedf835791a403d05a1619279e4e501fdb94
Adegoke Obasa <goke@cottacush.com>
2015-11-06 12:05:38 +0100

    Remove onforwarding charges from manage cities form fields

--- web/js/city.js
+++ web/js/city.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
         _id = $(this).attr('data-id');
         $(target+" input[name='city_name']").val($("td[class='n"+_id+"']").text());
         $(target+" input[name='transit_time']").val($("td[class='t"+_id+"']").text());
-        $(target+" select[name='charge']").val($(this).attr('data-charge-id'));
         $(target+" select[name='state']").val($(this).attr('data-state-id'));
         $(target+" select[name='branch_id']").val($(this).attr('data-branch-id'));
         $(target+" input[name='id']").val(_id);
