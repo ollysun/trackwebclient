488c6dd3fdee610d9f3c3902d0a326429f1bdc7c
Adegoke Obasa <goke@cottacush.com>
2015-11-08 20:40:37 +0100

    CPTTK-363 - Implement weight range creation for corporate billing

--- views/billing/corporate.php
+++ views/billing/corporate.php
@@ -16,87 +16,94 @@ $this->params['content_header_button'] = '<button type="button" class="btn btn-p
 ?>
 
 <?= Calypso::showFlashMessages(); ?>
-<div class="main-box">
-    <div class="main-box-header">
-    </div>
-    <div class="main-box-body">
-        <?php if (count($billingPlans) > 0): ?>
-            <table id="table" class="table table-bordered">
-                <thead>
-                <tr>
-                    <th>S/N</th>
-                    <th>Billing Name</th>
-                    <th>Associated Company</th>
-                    <th>Type</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $row = $offset;
-                foreach ($billingPlans as $billingPlan): ?>
+    <div class="main-box">
+        <div class="main-box-header">
+        </div>
+        <div class="main-box-body">
+            <?php if (count($billingPlans) > 0): ?>
+                <table id="table" class="table table-bordered">
+                    <thead>
                     <tr>
-                        <td><?= ++$row; ?></td>
-                        <td><?= strtoupper(Calypso::getValue($billingPlan, 'name')) ?></td>
-                        <td><?= strtoupper(Calypso::getValue($billingPlan, 'company.name', 'N/A')) ?></td>
-                        <td><?= strtoupper(Calypso::getValue(BillingPlanAdapter::getTypes(), Calypso::getValue($billingPlan, 'type'))); ?></td>
-                        <td></td>
+                        <th>S/N</th>
+                        <th>Billing Name</th>
+                        <th>Associated Company</th>
+                        <th>Type</th>
+                        <th>Actions</th>
                     </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-        <?php else: ?>
-            <div class="alert alert-info text-center" role="alert">
-                <p><strong>No billing plans have been created</strong></p>
-            </div>
-        <?php endif; ?>
-    </div>
-</div>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $row = $offset;
+                    foreach ($billingPlans as $billingPlan): ?>
+                        <tr>
+                            <td><?= ++$row; ?></td>
+                            <td><?= strtoupper(Calypso::getValue($billingPlan, 'name')) ?></td>
+                            <td><?= strtoupper(Calypso::getValue($billingPlan, 'company.name', 'N/A')) ?></td>
+                            <td><?= strtoupper(Calypso::getValue(BillingPlanAdapter::getTypes(), Calypso::getValue($billingPlan, 'type'))); ?></td>
+                            <td>
+                                <?php if (Calypso::getValue($billingPlan, 'type') == BillingPlanAdapter::TYPE_ON_FORWARDING): ?>
 
-<div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <form class="validate-form" method="post" action="<?= Url::to("/billing/savecorporate")?>">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Add Billing Plan</h4>
+                                <?php elseif (Calypso::getValue($billingPlan, 'type') == BillingPlanAdapter::TYPE_WEIGHT): ?>
+                                    <a href="<?= Url::to(["/billing/weightranges", 'billing_plan_id' => Calypso::getValue($billingPlan, 'id')])?>" class="btn btn-xs btn-default">Weight Ranges</a>
+                                    <a href="<?= Url::to(["/billing/pricing", 'billing_plan_id' => Calypso::getValue($billingPlan, 'id')])?>" class="btn btn-xs btn-primary">Billing</a>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            <?php else: ?>
+                <div class="alert alert-info text-center" role="alert">
+                    <p><strong>No billing plans have been created</strong></p>
                 </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input id="name" class="form-control validate required" name="name" />
-                    </div>
-                    <div class="form-group">
-                        <label for="">Associated Company</label>
-                        <select  name="company" class="form-control validate required">
-                            <option value="">Select Company</option>
-                            <?php foreach ($companies as $company): ?>
-                                <option
-                                    value="<?= Calypso::getValue($company, 'id'); ?>"><?= strtoupper(Calypso::getValue($company, 'name')) ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Type</label>
-                        <select  name="type" class="form-control validate required">
-                            <option value="">Select Type</option>
-                            <?php foreach ($billingPlanTypes as $id => $name): ?>
-                                <option
-                                    value="<?= $id ?>"><?= strtoupper($name) ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <input type="hidden" name="id">
-                    <input type="hidden" name="task" value="status">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </form>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
-<!-- this page specific scripts -->
+
+    <div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <form class="validate-form" method="post" action="<?= Url::to("/billing/savecorporate") ?>">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Add Billing Plan</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input id="name" class="form-control validate required" name="name"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Associated Company</label>
+                            <select name="company" class="form-control validate required">
+                                <option value="">Select Company</option>
+                                <?php foreach ($companies as $company): ?>
+                                    <option
+                                        value="<?= Calypso::getValue($company, 'id'); ?>"><?= strtoupper(Calypso::getValue($company, 'name')) ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Type</label>
+                            <select name="type" class="form-control validate required">
+                                <option value="">Select Type</option>
+                                <?php foreach ($billingPlanTypes as $id => $name): ?>
+                                    <option
+                                        value="<?= $id ?>"><?= strtoupper($name) ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="hidden" name="id">
+                        <input type="hidden" name="task" value="status">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- this page specific scripts -->
 <?php $this->registerJsFile('@web/js/validate.js', ['depends' => [\app\assets\AppAsset::className()]]) ?>
