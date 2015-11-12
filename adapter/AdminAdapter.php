807b576d8b3a377c29d1fd6291cb6669035a34f6
Babatunde Otaru <tunde@cottacush.com>
2015-11-12 17:16:01 +0100

    Implemeting reasons for returning a parcel.

--- adapter/AdminAdapter.php
+++ adapter/AdminAdapter.php
@@ -31,7 +31,8 @@ class AdminAdapter extends BaseAdapter{
 
     public function revalidate($staff=null, $password){
         $filter = is_null($staff) ? '':'identifier='.$staff;
-        $filter .= is_null($password) ? '':'&password='.$password;return  $this->request(ServiceConstant::URL_USER_VALIDATE.'?'.$filter, [], self::HTTP_POST);
+        $filter .= is_null($password) ? '':'&password='.$password;
+        return  $this->request(ServiceConstant::URL_USER_VALIDATE.'?'.$filter, [], self::HTTP_POST);
     }
 
     /**
