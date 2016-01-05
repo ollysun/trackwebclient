cca49914da6458f282c36a685428b74bf5b76126
tunde <tnde@babatunde>
2016-01-05 10:58:53 +0100

    add cherry picked commit - User jquery objects

--- views/billing/partial_billing_plans.php
+++ views/billing/partial_billing_plans.php
@@ -1,7 +1,7 @@
 <?php
 use Adapter\Util\Calypso;
 ?>
-<label for="">Billing lan name</label>
+<label for="">Billing Plan name</label>
 <select name="billing Plan" class="form-control validate required">
     <option value="">Select Company</option>
     <?php foreach ($billing_plans as $billing_plan): ?>
--- web/js/finance/corporate_billing.js
+++ web/js/finance/corporate_billing.js
@@ -1,15 +1,13 @@
 
-var checkbox = document.getElementById('clone-billing-plan');
-var cloneDetails = document.getElementById('clone-details');
 $("#clone-billing-plan").click(function () {
-    if (checkbox.checked) {
+    if ($("#clone-billing-plan").is(':checked')) {
         $.get("getallbillingplannames",'','html').success(function (data) {
             $("#clone-details").html(data);
         }).error(function(){
             $("#clone-details").html('something went wrong');
         });
     } else {
-        cloneDetails.innerHTML=" ";
+        $("#clone-details").html("");
     }
 });
 
