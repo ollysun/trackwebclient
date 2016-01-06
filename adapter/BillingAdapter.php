8e556f49b282207025bebf55af516b2686c0c6da
Adeyemi Olaoye <yemi@cottacush.com>
2016-01-06 14:19:00 +0100

    fix conflicts

--- adapter/BillingAdapter.php
+++ adapter/BillingAdapter.php
@@ -109,15 +109,4 @@ class BillingAdapter extends BaseAdapter
     }
 
     public function addException(){}
-
-    /**
-     * @author Babatunde Otaru <tunde@cottacush.com>
-     * @return array|mixed|string
-     */
-    public function getAllBillingPlanNames()
-    {
-        return $this->request(ServiceConstant::URL_GET_ALL_BILLING_PLAN_NAMES,[],self::HTTP_GET);
-    }
-
-
 }
