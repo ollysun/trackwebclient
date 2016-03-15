5f3ecb1f6c18dcaeb9098361d96261ed4377d33a
tunde <tnde@babatunde>
2016-03-15 22:04:10 +0100

    Check for edit access

--- views/elements/partial_edit_button.php
+++ views/elements/partial_edit_button.php
@@ -3,7 +3,7 @@ use Adapter\Util\Calypso;
 use Adapter\Globals\ServiceConstant;
 
 $branch_type = $this->context->userData['branch']['branch_type'];
-if(($parcel['is_related_to_this_branch'] && $branch_type == ServiceConstant::BRANCH_TYPE_HUB) || $branch_type == ServiceConstant::BRANCH_TYPE_HQ){
+if($parcel['edit_access'] || $branch_type == ServiceConstant::BRANCH_TYPE_HQ){
 ?>
 <a href=<?= '../parcels/new?edit=1&id=' . Calypso::getValue($parcel, 'id') ?>>
 <button class="btn btn-default btn-xs">Edit</button>
