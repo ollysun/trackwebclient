16f36c862d8cf164d9c3f59ff19eec12c9a46b72
tunde <tnde@babatunde>
2016-03-15 16:16:10 +0100

    Grant shipment edit access only to parent branch

--- views/elements/partial_edit_button.php
+++ views/elements/partial_edit_button.php
@@ -2,8 +2,8 @@
 use Adapter\Util\Calypso;
 use Adapter\Globals\ServiceConstant;
 
-
-if($parcel['is_related_to_this_branch'] || $this->context->userData['branch']['branch_type'] == ServiceConstant::BRANCH_TYPE_HQ){
+$branch_type = $this->context->userData['branch']['branch_type'];
+if(($parcel['is_related_to_this_branch'] && $branch_type == ServiceConstant::BRANCH_TYPE_HUB) || $branch_type == ServiceConstant::BRANCH_TYPE_HQ){
 ?>
 <a href=<?= '../parcels/new?edit=1&id=' . Calypso::getValue($parcel, 'id') ?>>
 <button class="btn btn-default btn-xs">Edit</button>
