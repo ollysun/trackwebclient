ff5c7cb61607f5e158c11c48ab631d3a924b3115
Adeyemi Olaoye <yemi@cottacush.com>
2016-04-06 17:00:01 +0100

    remove commented code

--- web/js/finance/corporate_shipment.js
+++ web/js/finance/corporate_shipment.js
@@ -78,19 +78,6 @@ $(document).ready(function () {
         $("input[name=parcel]").prop("checked", $(this).prop("checked"));
     });
 
-    $('.checkable').click(function (e) {
-        var _this = $(this);
-        var companyId = _this.data('company_id');
-        if (_this.is(':checked')) {
-            /*$('.checkable').each(function (i, v) {
-             if ($(v).is(':checked') && companyId != $(v).data('company_id')) {
-             e.preventDefault();
-             return false;
-             }
-             });*/
-        }
-    });
-
     $('#generateInvoiceBtn').unbind('click').click(function (e) {
         var proceed = false;
         $('.checkable').each(function (i, v) {
