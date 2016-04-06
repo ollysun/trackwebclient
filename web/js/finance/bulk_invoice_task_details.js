05735d8a1f5da811f3defc9a74aa7eb94f556ad8
Adegoke Obasa <goke@cottacush.com>
2016-04-06 17:21:55 +0100

    Fix issue with showing bulk invoice parcels

--- web/js/finance/bulk_invoice_task_details.js
+++ web/js/finance/bulk_invoice_task_details.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
         var parcelsContainer = $("#parcels").html(parcels.length == 0 ? 'No Parcels' : '');
         parcels.forEach(function (value) {
-            parcelsContainer.append('<li><a target="_blank" href="/shipments/view?waybill_number=' + value + ' ">' + value + '</a></li>')
+            parcelsContainer.append('<li><a target="_blank" href="/shipments/view?waybill_number=' + value.waybill_number + ' ">' + value.waybill_number + '</a></li>')
         });
     });
 });
