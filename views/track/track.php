4ef15b44641c87763ff3f6fb5b3b988d55dc3aaa
tunde <tnde@babatunde>
2016-04-21 16:09:39 +0100

    Add parcel not found message when tracking

--- views/track/track.php
+++ views/track/track.php
@@ -210,9 +210,8 @@ $this->title = 'Tracking Portal';
                 <p class="text-muted"> Sorry, You can't search for more than ten parcels </p>
             <?php elseif (is_array($tracking_info)): ?>
                 <h1>Waybill / Tracking Number <strong><?= $tracking_number ?></strong> not found</h1>
-                <p class="text-muted">The waybill / tracking number you entered could not be found on our
-                    system. Pleas
-                    search with another number</p>
+                <p class="text-muted">WayBill Number not recognized by the system please contact
+                    <a href=""> customerservice@courierplus-ng.com </a> for immediate resolution</p>
             <?php else: ?>
                 <h1>Error</h1>
                 <p class="text-muted">WayBill Number not recognized by the system please contact
