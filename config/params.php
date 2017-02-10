011d18602fde40f437e365bade172e18b842b218
Benedict Happy <Benedict Happy>
2017-02-10 17:07:51 +0100

    intl billing zone view

--- config/params.php
+++ config/params.php
@@ -4,6 +4,6 @@ return [
     //'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://54.93.116.160/",
     //'apiUrl' => "http://54.93.116.160/",
     //'apiUrl' => "http://52.29.170.65/",
-    'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://trackplus-server.com:82/",
+    'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://localhost:777/Trackplus/webservice/",
     'cacheAppPrefix' => 'track_plus::'
 ];
