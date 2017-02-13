56520853eca70ab297b073897aff21c630d941b1
Ademu Anthony <blenyo11@gmail.com>
2017-02-13 16:09:28 +0100

    intl weight range

--- adapter/Globals/ServiceConstant.php
+++ adapter/Globals/ServiceConstant.php
@@ -200,6 +200,7 @@ class ServiceConstant
     const URL_INTL_GET_COUNTRIES_BY_ZONE = 'intl/getCountriesByZone';
     const URL_MAP_COUNTY_TO_ZONE = 'intl/mapCountryToZone';
     const URL_INTL_ADD_WEIGHT_RANGE = 'intl/addWeightRange';
+    const URL_INTL_GET_WEIGHT_RANGE = 'intl/addWeightRange';
 
 
     //transit time
