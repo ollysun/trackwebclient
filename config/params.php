16db7bdc0d0cefacea1dc775d6d28d818cc523e9
Ademu Anthony <blenyo11@gmail.com>
2017-03-02 08:59:24 +0100

    param

--- config/params.php
+++ config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    //'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://54.93.116.160/",
+    'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://54.93.116.160/",
     //'apiUrl' => "http://54.93.116.160/",
     //'apiUrl' => "http://52.29.170.65/",
-    'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://trackplus-server.com/",
+    //'apiUrl' => getenv('API_URL') !== false ? getenv('API_URL') : "http://trackplus-server.com/",
     'cacheAppPrefix' => 'track_plus::'
 ];
